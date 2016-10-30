127078c5834cda0bcd5c98241fa061e4ea8c7229
mike <mikemay@blueyonder.co.uk>
2016-10-30 07:48:37 +0000

    MM; cosmetic

--- client/quotes/js/quotes.js
+++ client/quotes/js/quotes.js
@@ -8,14 +8,28 @@ var heatMapQuotesHandler_ns = (function() {
 
     function requestQuote() {
         var script = document.createElement("script");
-        script.src = 'http://frankfurt-rdp/quotes/generate_quotes.php' + '?cachebreaker=' + ii++;
+        script.src = 'http://localhost/quotes/generate_quotes.php' + '?cachebreaker=' + ii++;
+        // script.src = 'http://frankfurt-rdp/quotes/generate_quotes.php' + '?cachebreaker=' + ii++;
             // server returns "heatMapQuotesHandler_ns(...payload...);"
         document.getElementsByTagName("head")[0].appendChild(script);
     }
+
+    function displayError(msg) {
+        document.getElementById("PayloadError").innerHTML = msg;
+    }
+
     // payload: eg. { data: {stock: 'MSFT', price: 501.01, volume: 555444}}
     return function handler(payload) {
-        makeHeatMap(payload.data);
-        requestQuote();
+        if ( payload.data !== undefined) {
+            makeHeatMap(payload.data);
+            requestQuote();     // I think we can assume that all errors are irrecoverable
+        }
+        else if (payload.err !== undefined) {
+            displayError(payload.err);
+        }
+        else {
+            displayError("Invalid payload returned from server");
+        }
     }
 })();
 
--- server/generate_quotes.php
+++ server/generate_quotes.php
@@ -1,57 +1,65 @@
 <?php
-    // mike may, 28-Oct-2016
-    function getRandomDelay() {
-        static $normalishDistribution = array(
+// mike may, 28-Oct-2016
+function getRandomDelay() {
+    static $normalishDistribution = array(
         1 => 1
-        ,2 => 3
-        ,3 => 3
-        ,4 => 5
-        ,5 => 5
-        ,6 => 5
-        ,7 => 5
-        ,8 => 7
-        ,9 => 7
-        ,10 => 9
-        );
+    ,2 => 3
+    ,3 => 3
+    ,4 => 5
+    ,5 => 5
+    ,6 => 5
+    ,7 => 5
+    ,8 => 7
+    ,9 => 7
+    ,10 => 9
+    );
 
-        return $normalishDistribution[round(rand(1,10))];
-    }
-    $quoteQuery = "
-SELECT
-  quote_id,
-  stock,
-  time_indicator,
-  price,
-  volume
-FROM quotes.quotes
-WHERE quote_id =
-      CASE WHEN (SELECT last_quote
-                 FROM last_quote)
-                >= (SELECT max(quote_id)
-                    FROM quotes)
-        THEN (SELECT min(quote_id)
-              FROM quotes)
-      ELSE (SELECT min(quote_id)
-            FROM quotes
-            WHERE quote_id > (SELECT last_quote
-                              FROM last_quote)) END
+    return $normalishDistribution[round(rand(1,10))];
+}
+// get the next quote.  The id is provided by examining the value of
+// last_quote and finding the next in sequence or if we have reached the
+// end of the table then starting from the beginning
+$quoteQuery = "
+        SELECT
+          quote_id,
+          stock,
+          time_indicator,
+          price,
+          volume
+        FROM quotes.quotes
+        WHERE quote_id =
+              CASE WHEN (SELECT last_quote
+                         FROM last_quote)
+                        >= (SELECT max(quote_id)
+                            FROM quotes)
+                THEN (SELECT min(quote_id)
+                      FROM quotes)
+              ELSE (SELECT min(quote_id)
+                    FROM quotes
+                    WHERE quote_id > (SELECT last_quote
+                                      FROM last_quote)) END
 ";
-
+try {
     /** too much effort to install mysqli or PDO on this custom rolled v5.4 installation */
-    $conn = mysql_connect("localhost","quoter","thisusercando_nothing!!") or die("unable to connect to mysql on localhost");
-    mysql_select_db("quotes") or die("failed to find database quotes");
-
-//    $rs = mysql_query("select last_quote + 1 as last_quote_plus_one from last_quote where last_quote_id=1");
-
-//    $row = mysql_fetch_assoc($rs);
-
-//    $thisQuote = $row["last_quote_plus_one"];
-
-//    $rsQuote = mysql_query("select stock, time_indicator, price, volume from quotes.quotes where quotes.quote_id=" . $thisQuote);
-    $rsQuote = mysql_query($quoteQuery);
+    $host = "localhost"; $userName = "quoter"; $password = "thisusercando_nothing!!"; $db = "quotes";
+    if (!($conn = mysql_connect($host,$userName, $password))) {
+        throw new Exception("unable to connect to mysql on $host as user $userName. " . mysql_error($conn));
+    }
+    if (!mysql_select_db($db)) {
+        throw new Exception("missing database $db. " . mysql_error($conn));
+    }
+    if (!($rsQuote = mysql_query($quoteQuery))) {
+        throw new Exception("quotes query failed. " . mysql_error($conn));
+    }
     $rowQuote = mysql_fetch_assoc($rsQuote);
-    $res = mysql_query("update last_quote set last_quote=" . $rowQuote["quote_id"]);
+    if (!mysql_query("update last_quote set last_quote=" . $rowQuote["quote_id"])) {
+        throw new Exception("failed to update last_quote. " . mysql_error($conn));
+    }
     sleep(getRandomDelay());
-    print "heatMapQuotesHandler_ns({ data: {" . "stock:" . "'" . $rowQuote["stock"] ."'" . ",price:" . $rowQuote["price"] . ",volume:" . $rowQuote["volume"] . "}})";
+    print "heatMapQuotesHandler_ns({ data: {" . "stock:" . "'" . $rowQuote["stock"] . "'" . ",price:" . $rowQuote["price"] . ",volume:" . $rowQuote["volume"] . "}})";
     mysql_close($conn);
+}
+catch (Exception $ex) {
+    print "heatMapQuotesHandler_ns({ err: \"" . $ex->getMessage() . "\"})";
+}
 ?>
